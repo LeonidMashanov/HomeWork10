c6bf613c58cdbe25abad4ebb0ab9ad918bc5a082
LeonidMashanov <len4ik272523@gmail.com>
2020-12-29 11:41:04 +0200

    start Homework10

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    public void printElementsRightUp (int [][]arr){
+       System.out.println(arr[0][arr.length]);
+        System.out.println(arr[arr.length][0]);
+    }
+
+    public static void main(String[] args) {
+	int [][] array= new int [3][3];
+    }
+}
