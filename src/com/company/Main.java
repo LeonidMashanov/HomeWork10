a3cae279f62f22e565b01cb61eebb38888276e68
LeonidMashanov <len4ik272523@gmail.com>
2020-12-30 16:56:48 +0200

    make task1 and task2 of homework

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,58 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
-    public static void printElementsRightUpAndLeftDown (int [][]arr){
-       System.out.println(arr[0][arr.length-1]);
-        System.out.println(arr[arr.length-1][0]);
+    public static void printElementsRightUpAndLeftDown(int[][] arr) {
+        System.out.println(arr[0][arr.length - 1]);
+        System.out.println(arr[arr.length - 1][0]);
         return;
     }
-    public static void printElements5LineAnd2Column(int[][]arr){
+
+    public static void printElements5LineAnd2Column(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[5][i]);
+            System.out.println(arr[4][i]);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i][1]);
         }
     }
-    public static void main(String[] args) {
-	int [][] array= new int[6][6];
+
+
+
+    public static void randomNumbers(int[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
-                int ran = (int) (10*Math.random()*1);
+                int ran = (int) (10 * Math.random() * 1);
+                for (int k = 1; k < 10; k++) {
+
+                    int num = (int) (10 * Math.random() * 1);
+
+
+                }
+                array[i][j] = ran;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] array = new int[4][4];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                int ran = (int) (10 * Math.random() *1% 25);
                 array[i][j]= ran ;
             }
         }
 
-//        for (int i = 0; i < array.length; i++) {
-//            for (int j = 0; j < array.length; j++) {
-//                System.out.println(array[i][j]+"\t");
-//
-//            }
-//
-//        }
-     printElementsRightUpAndLeftDown(array);
-        printElements5LineAnd2Column(array);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.println(array[i][j]);
 
+            }
+
+        }
 
 
 
